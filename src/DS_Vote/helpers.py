20b16df59ec849e0db575e95e639d345559b4fa3
Csalex01 <csillag.alex@student.ms.sapientia.ro>
2024-03-18 19:13:12 +0200

    Added admin terminal user interface.

---
+++ src/DS_Vote/helpers.py
@@ -0,0 +1,15 @@
+import os
+
+class COLORS:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def clear_screen():
+  os.system('cls' if os.name == 'nt' else 'clear')
